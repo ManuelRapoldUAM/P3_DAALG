9bac86a6137caa373957fc7d39289fb364753b7c
Manuel Rapold <Manuel.Rapold@estudiante.uam.es>
2021-11-25 16:37:48 +0100

    Write file structure

---
+++ p311/fft_pdNN.py
@@ -0,0 +1,58 @@
+
+
+def fft(t):
+    return 0
+
+def invert_fft(t, fft_func = fft()):
+    return 0
+
+def rand_polynomial(long = 2**10, base = 10):
+    return 0
+
+def poli_2_num(l_pol, base = 10):
+    return 0
+
+def rand_number(num_digits, base = 10):
+    return 0
+
+def num_2_poli(num, base = 10):
+    return 0
+
+def mult_polynomials_fft(l_pol_1, l_pol_2, fft_func = fft):
+    return 0
+
+def mult_numbers_fft(num1, num2, fft_func = fft):
+    return 0
+
+def time_fft(n_tables, num_coefs_ini, num_coefs_fin, step, fft_func = fft):
+    return 0
+
+def time_mult_polynomials_fft(n_pairs, num_coefs_ini, num_coefs_fin, step, fft_func = fft):
+    return 0
+
+def time_mult_numbers_fft(n_ingers, num_digits_ini, num_digits_fin, step, fft_func = fft):
+    return 0
+
+def min_coin_number(c, l_coins):
+    return 0
+
+def optimal_change(c, l_coins):
+    return 0
+
+def bellman_ford(u, mg):
+    return 0
+
+def max_length_common_subsequence(str_1, str_2):
+    return 0
+
+def find_max_common_subsequence(str_1, str_2):
+    return 0
+
+def optimal_order(l_probs):
+    return 0
+
+def list_opt_ordering_search_tree(m_roots, l, r):
+    return 0
+
+if __name__ == "__main__":
+    print()
